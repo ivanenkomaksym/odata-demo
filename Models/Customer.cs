dbe644c783ce5b05e2ef84f6ba6b73f65dd3fb06
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-03-27 18:57:09 +0200

    Install OData. Add models and Customers controller.

---
+++ Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace odata_demo.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.ModelBuilder;
+using odata_demo.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var modelBuilder = new ODataConventionModelBuilder();
+modelBuilder.EntityType<Order>();
+modelBuilder.EntitySet<Customer>("Customers");
+
+builder.Services.AddControllers().AddOData(
+    options => options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(null).AddRouteComponents(
+        "odata",
+        modelBuilder.GetEdmModel()));
 
 var app = builder.Build();
 
@@ -11,5 +24,9 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 app.Run();
