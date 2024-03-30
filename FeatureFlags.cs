47428fce0edc2980232eca54b15f3e13df42c141
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-03-30 10:06:30 +0200

    Install omit null or default value serializer based on feature flags.

---
+++ FeatureFlags.cs
@@ -0,0 +1,8 @@
+﻿namespace ODataDemo
+{
+    public class FeatureFlags
+    {
+        public const string OmitNull = "OmitNull";
+        public const string OmitDefaultValue = "OmitDefaultValue";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.OData;
 using Microsoft.AspNetCore.OData.Formatter.Serialization;
+using Microsoft.FeatureManagement;
 using Microsoft.OData.ModelBuilder;
+using ODataDemo;
 using ODataDemo.Models;
 using ODataDemo.Serializers;
 
@@ -8,26 +11,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddFeatureManagement();
 
 var modelBuilder = new ODataConventionModelBuilder();
 modelBuilder.EntityType<Order>();
 modelBuilder.EntitySet<Customer>("Customers");
 
-builder.Services.AddControllers().AddOData(
-    options => options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(null).AddRouteComponents(
+builder.Services.AddControllers().AddOData(options =>
+    options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(null).AddRouteComponents(
         "odata",
         modelBuilder.GetEdmModel(),
-        // services => services.AddSingleton<ODataResourceSerializer, OmitNullResourceSerializer>()));
-        services => services.AddSingleton<ODataResourceSerializer, OmitPropertyWithDefaultValueResourceSerializer>(serviceProvider =>
-        {
-            var odataSerializerProvider = serviceProvider.GetRequiredService<IODataSerializerProvider>();
-            return new OmitPropertyWithDefaultValueResourceSerializer(odataSerializerProvider, new Dictionary<string, object> 
-            {
-                { nameof(Customer.StringPropertyWithDefaultValueToBeOmitted), string.Empty },
-                { nameof(Customer.BooleanPropertyWithDefaultValueToBeOmitted), false }
-            });
-        })));
+        ConfigureODataResourceSerializer(builder.Configuration)));
 
 var app = builder.Build();
 
@@ -42,3 +36,35 @@ app.UseRouting();
 app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 app.Run();
+
+static Action<IServiceCollection> ConfigureODataResourceSerializer(IConfiguration configuration)
+{
+    return services =>
+    {
+        // Need to install add Configuration and install feature management for AddRouteComponents separately
+        // https://stackoverflow.com/questions/59332463/unable-to-resolve-service-for-type-microsoft-extensions-configuration-iconfigura
+        services.AddSingleton(configuration).AddFeatureManagement();
+
+        services.AddSingleton(serviceProvider =>
+        {
+            var featureManager = serviceProvider.GetService<IFeatureManager>();
+            Debug.Assert(featureManager != null);
+            var odataSerializerProvider = serviceProvider.GetRequiredService<IODataSerializerProvider>();
+
+            if (featureManager.IsEnabledAsync(FeatureFlags.OmitNull).GetAwaiter().GetResult())
+            {
+                return new OmitNullResourceSerializer(odataSerializerProvider);
+            }
+            else if (featureManager.IsEnabledAsync(FeatureFlags.OmitDefaultValue).GetAwaiter().GetResult())
+            {
+                return new OmitPropertyWithDefaultValueResourceSerializer(odataSerializerProvider, new Dictionary<string, object>
+                {
+                    { nameof(Customer.StringPropertyWithDefaultValueToBeOmitted), string.Empty },
+                    { nameof(Customer.BooleanPropertyWithDefaultValueToBeOmitted), false }
+                });
+            }
+
+            return new ODataResourceSerializer(odataSerializerProvider);
+        });
+    };
+}
