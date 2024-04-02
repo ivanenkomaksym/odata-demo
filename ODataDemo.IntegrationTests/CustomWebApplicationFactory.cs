7c7abef7c02f133fa57d8ea09668d6dc2e69fbc7
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-04-02 09:51:37 +0300

    Add CustomWebApplicationFactory to be able to mock feature flags on startup.

---
+++ ODataDemo.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration.Memory;
+
+namespace ODataDemo.IntegrationTests
+{
+    internal class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        private readonly string FeatureFlag;
+
+        public CustomWebApplicationFactory(string featureFlag)
+        {
+            FeatureFlag = featureFlag;
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(configurationBuilder =>
+            {
+                configurationBuilder.Sources.Clear();
+
+                var configuration = new[] { KeyValuePair.Create<string, string?>($"FeatureManagement:{FeatureFlag}", "true") };
+                var memoryConfiguration = new MemoryConfigurationSource { InitialData = configuration };
+
+                configurationBuilder.Add(memoryConfiguration);
+            });
+        }
+    }
+}
--- ODataDemo.IntegrationTests/ResourceSerializerTests.cs
+++ ODataDemo.IntegrationTests/ResourceSerializerTests.cs
@@ -1,24 +1,17 @@
-using Microsoft.AspNetCore.Mvc.Testing;
-
 namespace ODataDemo.IntegrationTests
 {
-    public class ResourceSerializerTests : IClassFixture<WebApplicationFactory<Program>>
+    public class ResourceSerializerTests
     {
-        private readonly HttpClient _client;
-
-        public ResourceSerializerTests(WebApplicationFactory<Program> factory)
-        {
-            _client = factory.CreateClient();
-        }
-
         [Fact]
         public async Task GetWeatherForecat()
         {
             // Arrange
-            var url = "weatherforecast";
+            var factory = new CustomWebApplicationFactory(FeatureFlags.OmitNull);
+            var client = factory.CreateClient();
+            var url = "odata/Customers";
 
             // Act
-            var response = await _client.GetAsync(url);
+            var response = await client.GetAsync(url);
 
             // Assert
             response.EnsureSuccessStatusCode(); // Status Code 200-299
