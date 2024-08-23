36e56c732297e51bc40f18eb98a7657c76d84849
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-08-23 12:19:24 +0300

    Make customer repository configurable in tests.

--- ODataDemo.IntegrationTests/CustomWebApplicationFactory.cs
+++ ODataDemo.IntegrationTests/CustomWebApplicationFactory.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Configuration.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using NSubstitute;
-using ODataDemo.Models;
 using ODataDemo.Repository;
 
 namespace ODataDemo.IntegrationTests
@@ -12,10 +11,18 @@ namespace ODataDemo.IntegrationTests
     internal class CustomWebApplicationFactory : WebApplicationFactory<Program>
     {
         private readonly string FeatureFlag;
+        private readonly ICustomerRepository CustomerRepository;
 
         public CustomWebApplicationFactory(string featureFlag)
         {
             FeatureFlag = featureFlag;
+            CustomerRepository = new TestCustomerRepository();
+        }
+
+        public CustomWebApplicationFactory(string featureFlag, ICustomerRepository customerRepository)
+        {
+            FeatureFlag = featureFlag;
+            CustomerRepository = customerRepository;
         }
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
@@ -33,32 +40,11 @@ namespace ODataDemo.IntegrationTests
             builder.ConfigureServices(services =>
             {
                 var customerRepositoryMock = Substitute.For<ICustomerRepository>();
-                customerRepositoryMock.GetCustomers().Returns(GetMockCustomers());
+                customerRepositoryMock.GetCustomers().Returns(CustomerRepository.GetCustomers());
 
                 // Replace the ICustomerRepository registration with the mock
                 services.Replace(new ServiceDescriptor(typeof(ICustomerRepository), customerRepositoryMock));
             });
-        }
-
-        private static IQueryable<Customer> GetMockCustomers()
-        {
-            var mockCustomers = new List<Customer>
-            {
-                new() 
-                { 
-                    Id = 1,
-                    Name = "Mock Customer 1",
-                    UserRole = null,
-                },
-                new()
-                {
-                    Id = 2,
-                    Name = "Mock Customer 2",
-                    UserRole = null,
-                }
-            };
-
-            return mockCustomers.AsQueryable();
         }
     }
 }
---
+++ ODataDemo.IntegrationTests/TestCustomerRepository.cs
@@ -0,0 +1,29 @@
+﻿using ODataDemo.Models;
+using ODataDemo.Repository;
+
+namespace ODataDemo.IntegrationTests
+{
+    internal class TestCustomerRepository : ICustomerRepository
+    {
+        public IQueryable<Customer> GetCustomers()
+        {
+            var mockCustomers = new List<Customer>
+            {
+                new()
+                {
+                    Id = 1,
+                    Name = "Mock Customer 1",
+                    UserRole = null,
+                },
+                new()
+                {
+                    Id = 2,
+                    Name = "Mock Customer 2",
+                    UserRole = null,
+                }
+            };
+
+            return mockCustomers.AsQueryable();
+        }
+    }
+}
