bfab2eed7f07b072bbf322a7d5a680712cf01194
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-08-28 09:43:11 +0300

    Add tests to cover $select and $expand clauses.

--- ODataDemo.IntegrationTests/QueryParamsTests.cs
+++ ODataDemo.IntegrationTests/QueryParamsTests.cs
@@ -1,4 +1,5 @@
-﻿using ODataDemo.Models;
+﻿using ODataDemo.Extensions;
+using ODataDemo.Models;
 using ODataDemo.Repository;
 using System.Text.Json;
 using System.Text.Json.Nodes;
@@ -41,6 +42,56 @@ namespace ODataDemo.IntegrationTests
             Assert.Equal(2, customers.Count());
             var matchedCustomers = customers.Where(c => ids.Contains(c.Name));
             Assert.Equal(2, matchedCustomers.Count());
+        }
+
+        [Fact]
+        public async Task SelectClauseShouldSelect()
+        {
+            // Arrange
+            var ids = new List<string> { "Customer 1", "Customer 3" };
+            var factory = new CustomWebApplicationFactory(FeatureFlags.FilterQueryOptionRequired, new StaticCustomerRepository());
+            var client = factory.CreateClient();
+            var url = $"odata/Customers?$filter=Name in ('{ids[0]}', '{ids[1]}')&$select=id,name";
+
+            // Act
+            var response = await client.GetAsync(url);
+            var responseAsStr = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Contains(nameof(Customer.Id).ToCamelCase(), responseAsStr);
+            Assert.Contains(nameof(Customer.Name).ToCamelCase(), responseAsStr);
+            Assert.DoesNotContain(nameof(Customer.UserRole).ToCamelCase(), responseAsStr);
+            Assert.DoesNotContain(nameof(Customer.Orders).ToCamelCase(), responseAsStr);
+            Assert.DoesNotContain(nameof(Customer.StringPropertyWithDefaultValueToBeOmitted).ToCamelCase(), responseAsStr);
+            Assert.DoesNotContain(nameof(Customer.BooleanPropertyWithDefaultValueToBeOmitted).ToCamelCase(), responseAsStr);
+            Assert.DoesNotContain(nameof(Customer.Contract).ToCamelCase(), responseAsStr);
+        }
+
+        [Fact]
+        public async Task ExpandClauseShouldExpand()
+        {
+            // Arrange
+            var ids = new List<string> { "Customer 1", "Customer 3" };
+            var factory = new CustomWebApplicationFactory(FeatureFlags.FilterQueryOptionRequired, new StaticCustomerRepository());
+            var client = factory.CreateClient();
+            var urlWithExpandContract = $"odata/Customers?$filter=Name in ('{ids[0]}', '{ids[1]}')&$expand=contract";
+            var urlWithExpandContractAndAddress = $"odata/Customers?$filter=Name in ('{ids[0]}', '{ids[1]}')&$expand=contract($expand=address)";
+
+            // Act
+            var responseWithExpandContract = await client.GetAsync(urlWithExpandContract);
+            var responseWithExpandContractAsStr = await responseWithExpandContract.Content.ReadAsStringAsync();
+
+            var responseWithExpandContractAndAddress = await client.GetAsync(urlWithExpandContractAndAddress);
+            var responseWithExpandContractAndAddressAsStr = await responseWithExpandContractAndAddress.Content.ReadAsStringAsync();
+
+            // Assert
+            responseWithExpandContract.EnsureSuccessStatusCode();
+            Assert.Contains(nameof(Customer.Contract).ToCamelCase(), responseWithExpandContractAsStr);
+            Assert.DoesNotContain(nameof(IContract.Address).ToCamelCase(), responseWithExpandContractAsStr);
+
+            responseWithExpandContractAndAddress.EnsureSuccessStatusCode();
+            Assert.Contains(nameof(IContract.Address).ToCamelCase(), responseWithExpandContractAndAddressAsStr);
         }
     }
 }
