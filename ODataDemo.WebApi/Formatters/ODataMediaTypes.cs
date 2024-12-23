06bad28d4d7f926ed5612b69be83a098d45bc9fa
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-12-23 12:15:01 +0200

    Add ProducesODataAttribute. Make it return different list of accepted types based on dev or prod environment.

---
+++ ODataDemo.WebApi/Formatters/ODataMediaTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace ODataDemo.Formatters
+{
+    internal static class ODataMediaTypes
+    {
+        public static string ApplicationJsonODataMinimalMetadata = "application/json;odata.metadata=minimal";
+        public static string ApplicationJsonODataFullMetadata = "application/json;odata.metadata=full";
+        public static string ApplicationJsonODataNoMetadata = "application/json;odata.metadata=none";
+    }
+}
---
+++ ODataDemo.WebApi/Formatters/ProducesODataAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ODataDemo.Formatters
+{
+    public class ProducesODataAttribute : ProducesAttribute
+    {
+        public ProducesODataAttribute(IHostEnvironment environment)
+            : base(MediaTypeNames.Application.Json, GetAcceptedTypes(environment))
+        {
+        }
+
+        private static string[] GetAcceptedTypes(IHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                return [ODataMediaTypes.ApplicationJsonODataFullMetadata,
+                        ODataMediaTypes.ApplicationJsonODataMinimalMetadata,
+                        ODataMediaTypes.ApplicationJsonODataNoMetadata];
+            }
+
+            return [ODataMediaTypes.ApplicationJsonODataNoMetadata];
+        }
+    }
+}
